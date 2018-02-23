fefc261aa4d3a80d096ad5347bcae9b4b7f7ddd0
Jurgen Kluft <jurgen.kluft@gmail.com>
2018-02-23 18:15:36 +0800

    * implementation WIP

--- source/main/cpp/x_actor.cpp
+++ source/main/cpp/x_actor.cpp
@@ -1,25 +1,15 @@
+#include "xbase/x_allocator.h"
 #include "xactor/x_actor.h"
+
+#ifdef TARGET_PC
+	#include <windows.h>
+	#include <stdio.h>
+	#include <atomic>
+#endif
 
 
 namespace xcore
 {
-	typedef		u64		msg_id_t;
-	typedef		u64		actor_id_t;
-
-	// For messages we can have one allocator per actor for sending messages.
-	// This makes the actor be able to control/limit the messages that it
-	// creates and sends.
-	// The necessary information for a messages is where the message came
-	// from so that the receiving actor can send a message back to the
-	// sender.
-	class xmessage
-	{
-	protected:
-		msg_id_t		m_id;
-		xactor*			m_dst;
-		xactor*			m_src;
-	};
-
 	// Handling a message and figuring out what function to call on the
 	// receiving actor.
 	// An actor can register message handlers by first registering the
@@ -28,159 +18,389 @@ namespace xcore
 	class my_message : public xmessage
 	{
 	protected:
-		XACTOR_DECLARE_MESSAGE(my_message);
+		//XACTOR_DECLARE_MESSAGE(my_message);
 
-		s32	m_my_data1;
-		f32 m_my_data2;
+		s32					m_my_data1;
+		f32					m_my_data2;
 
 	public:
-
 		
+	};
+
+	class s32atomic
+	{
+		std::atomic<s32> m_value;
+	public:
+		s32				load() { return m_value.load(); }
+		bool			cas(s32 old, s32 _new) { return m_value.compare_exchange_weak(old, _new); }
+	};
+
+	class s64atomic
+	{
+		std::atomic<s64> m_value;
+
+	public:
+		s64				load() { return m_value.load(); }
+		bool			cas(s64 old, s64 _new) { return m_value.compare_exchange_weak(old, _new); }
 	};
 
 	// We base the receiving of messages on simple structs, messages are
 	// always send back to the sender for garbage collection to simplify
 	// creation, re-use and destruction of messages.
 
-	class xactor
-	{
-	public:
-		virtual void	process(xmessage* msg) = 0;
-		virtual void	gc(xmessage* msg) = 0;
-	};
-
-	class xwork
-	{
-	public:
-		virtual bool	take(xactor*& actor, xmessage*& msg, void*& tag) = 0;
-		virtual void	done(xactor* actor, xmessage* msg, void* tag) = 0;
-	};
-
 	// There are a fixed number of worker-threads, initialized according to what
 	// the user needs. The user can use the xsystem package to identify how
 	// many physical and logical cores this machine has as well as how many
 	// hardware threads.
 
-	class xworker
+	// C 1 | R 20 | RW 20 | W 20
+
+	const u64 WRITE_INDEX_SHIFT = 0;
+	const u64 WRITE_INDEX_BITS = 20;
+	const u64 WRITE_INDEX_MASK = ((u64(1) << WRITE_INDEX_BITS) - 1) << WRITE_INDEX_SHIFT;
+	const u64 READ_INDEX_SHIFT = 20;
+	const u64 READ_INDEX_BITS = 20;
+	const u64 READ_INDEX_MASK = ((u64(1) << READ_INDEX_BITS) - 1) << READ_INDEX_SHIFT;
+	const u64 QUEUED_FLAG_SHIFT = 62;
+	const u64 QUEUED_FLAG = u64(1) << QUEUED_FLAG_SHIFT;
+
+	class xqueue
 	{
+		u32					m_size;
+		void**				m_queue;
+		s64atomic*			m_index;
+
 	public:
-		virtual void		run(xwork* work) = 0;
+		void				init(void** queue, s32 size)
+		{
+			m_queue = queue;
+			m_size = size;
+			m_index = 0;
+		}
+
+		virtual s32			push(void* p)
+		{
+			// We are pushing the message when the actor is QUEUED or NOT-QUEUED.
+			s64 i, n;
+			do {
+				i = m_index->load();
+				n = (i + 1) | QUEUED_FLAG;
+			} while (!m_index->cas(i,n));
+
+			m_queue[i & (m_size-1)] = p;
+
+			// Did we push a message and the actor is not queued ?
+			// If so we need to signal the caller that it should
+			// queue the actor. We already set the QUEUED_FLAG.
+			s32 const queue = ((i & QUEUED_FLAG) == (n & QUEUED_FLAG)) ? 0 : 1;
+			return queue;
+		}
+
+		virtual void		pop(void*& p)
+		{
+			s64 i, n;
+			do {
+				i = m_index->load();
+				n = i + (s64(1) << READ_INDEX_SHIFT);
+			} while (!m_index->cas(i, n));
+			p = m_queue[(i - 1) & (m_size - 1)];
+		}
+
+		virtual void		claim(u32& idx, u32& end)
+		{
+			s64 i = m_index->load();
+			idx = u32((i & READ_INDEX_MASK) >> READ_INDEX_SHIFT);
+			end = u32((i & WRITE_INDEX_MASK) >> WRITE_INDEX_SHIFT);
+		}
+
+		virtual void		deque(u32& idx, u32 end, void*& p)
+		{
+			p = m_queue[idx & (m_size-1)];
+			idx += 1;
+		}
+
+		virtual s32			release(u32 idx, u32 end)
+		{
+			// This is our new 'read' index
+			bool const do_mask = (end >= m_size);
+			u32 const read = do_mask ? end & (m_size - 1) : end;
+
+			s64 i,n;
+			do {
+				i = m_index->load();
+				u32 write = u32((i & WRITE_INDEX_MASK) >> WRITE_INDEX_SHIFT);
+				if (do_mask)
+				{
+					write = write && (m_size - 1);
+				}
+				n = ((read & READ_INDEX_MASK) << READ_INDEX_SHIFT) | (write << WRITE_INDEX_SHIFT);
+				if (read < write)
+				{	// Let ourselves be queued since we have more messages to process
+					n |= QUEUED_FLAG;
+				}
+			} while (!m_index->cas(i, n));
+
+			// So we now have updated 'm_index' with 'read'/'write' and have succesfully updated the QUEUED_FLAG (set or unset)
+			return (n & QUEUED_FLAG) == 0 ? 0 : 1;
+		}
 	};
 
-	class xengine
+	class xmessageslf : public xmessages
 	{
+		xqueue		m_queue;
+	
 	public:
-		virtual void		start() = 0;
-		virtual void		stop() = 0;
+		virtual s32			push(xmessage* msg)
+		{
+			void * p = (void *)msg;
+			return m_queue.push(p);
+		}
+
+		virtual void		claim(u32& idx, u32& end)
+		{
+			m_queue.claim(idx, end);
+		}
+
+		virtual void		deque(u32& idx, u32 end, xmessage*& msg)
+		{
+			void* p;
+			m_queue.deque(idx, end, p);
+			msg = (xmessage*)p;
+		}
+
+		virtual s32			release(u32 idx, u32 end)
+		{
+			return m_queue.release(idx, end);
+		}
 	};
 
-	class xsystem
+	class xactor_mailbox : public xmailbox
 	{
 	public:
-		virtual void		join(actor_id_t id, xactor* actor) = 0;
-		virtual void		leave(actor_id_t id, xactor* actor) = 0;
+		void				initialize(xactor* actor, xwork* work, x_iallocator* allocator, s32 max_messages)
+		{
+			m_actor = actor;
+			m_work = work;
 
-		virtual void		send(actor_id_t to, xmessage* msg) = 0;
+			x_type_allocator<xmessageslf> xmessages_type(allocator);
+			m_messages = xmessages_type.allocate();
+
+		}
+
+		xactor *			m_actor;
+		xwork *				m_work;
+		virtual void		send(xmessage* msg, xactor* recipient)
+		{
+			m_work->add(m_actor, msg, recipient);
+		}
+
+		s32					push(xmessage* msg);
+		void				claim(u32& idx, u32& end);					// return [i,end] range of messages
+		void				deque(u32& idx, u32 end, xmessage*& msg);
+		s32					release(u32 idx, u32 end);				// return 1 when there are messages pending
+
+		xmessages *			m_messages;
 	};
 
-	class xthread_control
+	s32		xactor_mailbox::push(xmessage* msg)
 	{
+		if (m_messages->push(msg) > 0) {
+			return 1;
+		}
+		return 0;
+	}
+
+	void	xactor_mailbox::claim(u32& idx, u32& end)
+	{
+		m_messages->claim(idx, end);
+	}
+
+	void	xactor_mailbox::deque(u32& idx, u32 end, xmessage*& msg)
+	{
+		m_messages->deque(idx, end, msg);
+	}
+
+	s32		xactor_mailbox::release(u32 idx, u32 end)
+	{
+		if (m_messages->release(idx, end) > 0) {
+			return 1;
+		}
+		return 0;
+	}
+
+	class xsemaphore_imp : public xsemaphore
+	{
+		HANDLE				ghSemaphore;
+
 	public:
-		virtual void		sleep(u64 time_us) = 0;
-		virtual void		yield() = 0;
-		virtual void		exit() = 0;
+		virtual void		setup(s32 initial, s32 maximum)
+		{
+			ghSemaphore = ::CreateSemaphore(
+				NULL,           // default security attributes
+				initial,		// initial count
+				maximum,		// maximum count
+				NULL);          // unnamed semaphore
+		}
+
+		virtual void		teardown()
+		{
+			CloseHandle(ghSemaphore);
+		}
+
+		virtual void		request()
+		{
+			DWORD dwWaitResult = WaitForSingleObject(ghSemaphore, INFINITE);
+			switch (dwWaitResult)
+			{
+			case WAIT_OBJECT_0:	// The semaphore object was signaled.
+
+				break;
+			case WAIT_FAILED:
+				break;
+			}
+		}
+
+		virtual void		release()
+		{
+			::ReleaseSemaphore( ghSemaphore/*handle to semaphore*/, 1 /*increase count by one*/, NULL);
+		}
+
+		XCORE_CLASS_PLACEMENT_NEW_DELETE
 	};
 
-	class xthread_functor
+	class xwork_queue
 	{
+		xqueue				m_queue;
+
 	public:
-		virtual s32			run(xthread_control* tc) = 0;
-	};
+		void				initialize(x_iallocator* allocator, s32 max_actors)
+		{
+			void** queue = (void**)allocator->allocate(sizeof(void*) * max_actors, sizeof(void*));
+			m_queue.init(queue, max_actors);
 
-	class xthreads
-	{
-	public:
-		virtual void		create(xthread*& ) = 0;
-		virtual void		teardown(xthread* ) = 0;
+			x_type_allocator<xsemaphore_imp> sema_type(allocator);
+			m_sema = sema_type.allocate();
+		}
 
-		virtual void		start(xthread*, xthread_functor*) = 0;
-		virtual void		stop(xthread*) = 0;
+		void				push(xactor* actor)
+		{
+			void * p = (void *)actor;
+			m_queue.push(p);
+			m_sema->release();
+		}
 
-		virtual void		exit() = 0;
-		virtual xthread*	current() = 0;
+		void				pop(xactor*& actor)
+		{
+			void * p;
+			m_sema->request();
+			m_queue.pop(p);
+			actor = (xactor*)p;
+		}
 
-		static xthreads*	instance();
-	};
-
-	class xthread
-	{
-	public:
-		virtual u64			get_tid() const = 0;
-		virtual u32			get_idx() const = 0;
-		virtual const char*	get_name() const = 0;
-		virtual u64			get_priority() const = 0;
-		virtual u64			get_stacksize() const = 0;
-
-		virtual void*		set_tls_slot(s32 slot, void*) = 0;
-		virtual void*		get_tls_slot(s32 slot) = 0;
-
-	protected:
-		virtual void		set_tid(u64 tid) = 0;
-		virtual void		set_idx(u32 idx) = 0;
-		virtual void		set_name(const char*) = 0;
-		virtual void		set_priority(u64 size) = 0;
-		virtual void		set_stacksize(u64 size) = 0;
-		virtual void		set_tls_storage(s32 max_slots, void** storage) = 0;
-
-		virtual s32			run(xthread_functor* f) = 0;
-		virtual void		stop() = 0;
-
-		virtual void		start() = 0;
+		xsemaphore*			m_sema;
 	};
 
 	class xwork_imp : public xwork
 	{
+		xwork_queue			m_queue;
+
 	public:
-		void		init(s32 max_actors)
+		void				init(s32 max_actors)
 		{
 			// Initialize a ring buffer that can hold 'max_actors' and will act as the
 			// actual work queue.
 			// Create semaphore with 'max_actors' as a maximum for the counter.
 		}
 
-		bool		take(xthread* thread, xactor*& actor, xmessage*& msg, void*& arg)
+		// @Note: This can be called from multiple threads!
+		void				add(xactor* sender, xmessage* msg, xactor* recipient)
 		{
-			// The argument 'arg' is set when actor!=NULL and the message queue of 
-			// the 'actor' still has messages. 'arg' has to be passed when calling
-			// 'done()'
-
-			// If 'actor-still-has-messages'==false and the work-queue is empty then return 
-			// false and the thread will go into the wait() function of the queue-semaphore.
+			xactor_mailbox* mb = static_cast<xactor_mailbox*>(recipient->getmailbox());
+			if (mb->push(msg) == 1) 
+			{
+				// mailbox indicated that we have pushed a message and the actor is already
+				// marked as 'idle' and we are the one here to push him in the work-queue.
+				m_queue.push(recipient);
+			}
 		}
 
-		void		done(xactor* actor, xmessage* msg, void* arg)
+		void				take(xworker_thread* thread, xactor*& actor, xmessage*& msg, u32& msgidx, u32& msgend)
 		{
-			// If boolean 'add_back_to_workqueue' is true add the actor back to the work-queue
-			// Deallocate the msg
+			if (actor == NULL)
+			{
+				// This will make the calling thread block if the queue is empty
+				m_queue.pop(actor);
+
+				xactor_mailbox* mb = static_cast<xactor_mailbox*>(actor->getmailbox());
+				mb->claim(msgidx, msgend);
+				mb->deque(msgidx, msgend, msg);
+			}
+			else
+			{
+				// Take the next message from the mailbox of the actor
+				xactor_mailbox* mb = static_cast<xactor_mailbox*>(actor->getmailbox());
+				mb->deque(msgidx, msgend, msg);
+			}
 		}
 
-
-		// 
-		// External calls
-		// Send message to actor
-		//
-
-		/*
-			A pre-allocated ring-buffer of MAX-ACTORS should be perfect for the work-queue.
-		*/
-
-		void		send(xactor* actor, xmessage* msg)
+		void				done(xworker_thread* thread, xactor*& actor, xmessage*& msg, u32& msgidx, u32& msgend)
 		{
-			// This function can be called from multiple threads!
-			//    When the message queue size of the actor goes from 0 -> 1 add the actor to the work-queue
-			//    and call signal() on the semaphore.
+			// If 'msgidx == msgend' then try and add the actor back to the work-queue since
+			// it was the last message we supposed to have processed.
+			if (msgidx == msgend)
+			{
+				xactor_mailbox* mb = static_cast<xactor_mailbox*>(actor->getmailbox());
+				if (mb->release(msgidx, msgend) == 1)
+				{
+					// mailbox indicated that we have to push back the actor in the work-queue
+					// because there are new messages pending.
+					m_queue.push(actor);
+				}
+				actor = NULL;
+			}
+			msg = NULL;
 		}
 
+	};
+
+	class xworker_imp: public xworker
+	{
+	public:
+		void				run(xworker_thread* thread, xwork* work)
+		{
+			u32 i, e;
+			xactor* actor;
+			xmessage* msg;
+
+			while (thread->quit()==false)
+			{
+				// Try and take an [actor, message] piece of work
+				work->take(thread, actor, msg, i, e);
+				
+				// Let the actor handle the message
+				if (msg->is_recipient(actor))
+				{
+					actor->process(msg);
+					if (msg->is_sender(actor))
+					{
+						// Garbage collect the message immediately
+						actor->gc(msg);
+					}
+					else 
+					{
+						// Send this message back to sender
+						work->add(msg->get_recipient(), msg, msg->get_sender());
+					}
+				}
+				else if (msg->is_sender(actor))
+				{
+					actor->gc(msg);
+				}
+
+				// Report the [actor, message] back as 'done'
+				work->done(thread, actor, msg, i, e);
+			}
+		}
 	};
 
 }
--- source/main/include/xactor/x_actor.h
+++ source/main/include/xactor/x_actor.h
@@ -8,11 +8,168 @@
 
 namespace xcore
 {
+	typedef		u64		msg_id_t;
+	typedef		u64		actor_id_t;
+
+	class xmessage;
+	class xmessages;
+	class xactor;
+	class xworker_thread;
+	class xwork;
+	class xthread;
+
+	// For messages we can have one allocator per actor for sending messages.
+	// This makes the actor be able to control/limit the messages that it
+	// creates and sends.
+	// The necessary information for a messages is where the message came
+	// from so that the receiving actor can send a message back to the
+	// sender.
+	class xmessage
+	{
+	public:
+		bool				is_sender(xactor* s) const		{ return m_sender == s; }
+		bool				is_recipient(xactor* r) const	{ return m_recipient == r; }
+
+		xactor*				get_sender() const				{ return m_sender; }
+		xactor*				get_recipient() const			{ return m_recipient; }
+
+	protected:
+		msg_id_t			m_id;
+		xactor*				m_sender;
+		xactor*				m_recipient;
+	};
+
+
+	class xmailbox
+	{
+	public:
+		virtual void		send(xmessage* msg, xactor* recipient) = 0;
+	};
+
+	class xmessages
+	{
+	public:
+		virtual s32			push(xmessage* msg) = 0;						// return==1 size was '0' before push
+		virtual void		claim(u32& idx, u32& end) = 0;					// claim a message batch
+		virtual void		deque(u32& idx, u32 end, xmessage*& msg) = 0;	// next message from the claimed batch
+		virtual s32			release(u32 idx, u32 end) = 0;					// release batch, return 'count'
+	};
+
 	class xactor
 	{
 	public:
+		virtual void		setmailbox(xmailbox* mailbox) = 0;
+		virtual xmailbox*	getmailbox() = 0;
 
+		virtual void		process(xmessage* msg) = 0;
+		virtual void		gc(xmessage*& msg) = 0;
 	};
+
+	class xworker_thread
+	{
+	public:
+		virtual bool		quit() const = 0;
+	};
+
+	class xwork
+	{
+	public:
+		virtual void		add(xactor* sender, xmessage* msg, xactor* recipient) = 0;
+
+		virtual void		queue(xactor* actor) = 0;
+		virtual void		take(xworker_thread* thread, xactor*& actor, xmessage*& msg, u32& idx_begin, u32& idx_end) = 0;
+		virtual void		done(xworker_thread* thread, xactor*& actor, xmessage*& msg, u32& idx_begin, u32& idx_end) = 0;
+	};
+
+
+	class xworker
+	{
+	public:
+		virtual void		run(xworker_thread*, xwork*) = 0;
+	};
+
+
+	class xsystem
+	{
+	public:
+		virtual void		start() = 0;
+		virtual void		stop() = 0;
+
+		virtual void		join(xactor* actor) = 0;
+		virtual void		leave(xactor* actor) = 0;
+
+		virtual void		send(xmessage* msg, xactor* recipient) = 0;
+	};
+
+
+	class xthread_control
+	{
+	public:
+		virtual void		sleep(u64 time_us) = 0;
+		virtual void		yield() = 0;
+		virtual void		exit() = 0;
+	};
+
+	class xthread_functor
+	{
+	public:
+		virtual s32			run(xthread_control* tc) = 0;
+	};
+
+	class xthreads
+	{
+	public:
+		virtual void		create(xthread*&) = 0;
+		virtual void		teardown(xthread*) = 0;
+
+		virtual void		start(xthread*, xthread_functor*) = 0;
+		virtual void		stop(xthread*) = 0;
+
+		virtual void		exit() = 0;
+		virtual xthread*	current() = 0;
+
+		static void			set_instance(xthreads*);
+		static xthreads*	get_instance();
+	};
+
+	class xthread
+	{
+	public:
+		virtual u64			get_tid() const = 0;
+		virtual u32			get_idx() const = 0;
+		virtual void		get_name(const char*&) const = 0;
+		virtual u64			get_priority() const = 0;
+		virtual u64			get_stacksize() const = 0;
+
+		virtual void*		set_tls_slot(s32 slot, void*) = 0;
+		virtual void*		get_tls_slot(s32 slot) = 0;
+
+	protected:
+		virtual void		set_tid(u64 tid) = 0;
+		virtual void		set_idx(u32 idx) = 0;
+		virtual void		set_name(const char*) = 0;
+		virtual void		set_priority(u64 size) = 0;
+		virtual void		set_stacksize(u64 size) = 0;
+		virtual void		set_tls_storage(s32 max_slots, void** storage) = 0;
+
+		virtual s32			run(xthread_functor* f) = 0;
+		virtual void		stop() = 0;
+
+		virtual void		start() = 0;
+	};
+
+	class xsemaphore
+	{
+	public:
+		virtual void		setup(s32 count) = 0;
+		virtual void		teardown() = 0;
+
+		virtual void		request() = 0;
+		virtual void		release() = 0;
+	};
+
+
+	void				send(xactor* sender, xmessage* msg, xactor* recipient);
 
 } // namespace xcore
 
