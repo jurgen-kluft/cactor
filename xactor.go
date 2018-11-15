a255afca55a658e8e364ac30c111632f89ef39e8
Jurgen Kluft <jurgen.kluft@gmail.com>
2018-11-15 20:15:02 +0800

    * adjusted for xcode/tundra

--- xactor.go
+++ xactor.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/jurgen-kluft/xcode"
 	"github.com/jurgen-kluft/xactor/package"
+	"github.com/jurgen-kluft/xcode"
 )
 
 func main() {
+	xcode.Init()
 	xcode.Generate(xactor.GetPackage())
 }
