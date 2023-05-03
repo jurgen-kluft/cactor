42bf0976a2ad8799a3051d2f524b9ea58e483639
jurgen.kluft <jurgen.kluft@gmail.com>
2023-05-03 13:26:54 +0800

    * package; fix (removal of centry, not needed for cunittest)

--- package/package.go
+++ package/package.go
@@ -3,7 +3,6 @@ package cactor
 import (
 	cbase "github.com/jurgen-kluft/cbase/package"
 	"github.com/jurgen-kluft/ccode/denv"
-	centry "github.com/jurgen-kluft/centry/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
 )
 
@@ -11,13 +10,11 @@ import (
 func GetPackage() *denv.Package {
 	// Dependencies
 	cunittestpkg := cunittest.GetPackage()
-	centrypkg := centry.GetPackage()
 	cbasepkg := cbase.GetPackage()
 
 	// The main (cactor) package
 	mainpkg := denv.NewPackage("cactor")
 	mainpkg.AddPackage(cunittestpkg)
-	mainpkg.AddPackage(centrypkg)
 	mainpkg.AddPackage(cbasepkg)
 
 	// 'cactor' library
@@ -27,7 +24,6 @@ func GetPackage() *denv.Package {
 	// 'cactor' unittest project
 	maintest := denv.SetupDefaultCppTestProject("cactor_test", "github.com\\jurgen-kluft\\cactor")
 	maintest.Dependencies = append(maintest.Dependencies, cunittestpkg.GetMainLib())
-	maintest.Dependencies = append(maintest.Dependencies, centrypkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, cbasepkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
